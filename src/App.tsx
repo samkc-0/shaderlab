bb9941bf3eef493e9fa8f9ee3cfc0741f4de3100
samkc-0 <sam.k.c.0@icloud.com>
2025-11-27 00:11:43 +0000

    add default shaders

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,12 @@ import { OrbitControls } from "@react-three/drei";
 import { highlightGLSL as highlight } from "./highlight-glsl";
 
 export function App() {
-  const [vertexShader, setVertexShader] = useState<string>("");
-  const [fragmentShader, setFragmentShader] = useState<string>("");
+  const [vertexShader, setVertexShader] = useState<string>(
+    getDefaultVertexShader(),
+  );
+  const [fragmentShader, setFragmentShader] = useState<string>(
+    getDefaultFragmentShader(),
+  );
 
   return (
     <main className="grid-container fixed top-0 left-0 w-full h-full mx-auto text-center z-10 bg-lime-50">
@@ -44,10 +48,43 @@ const Preview = memo(function Preview({
       <OrbitControls />
       <mesh>
         <planeGeometry args={[1, 1, 32, 32]} />
-        <meshBasicMaterial />
+        {fragmentShader ? (
+          <shaderMaterial
+            vertexShader={vertexShader}
+            fragmentShader={fragmentShader}
+          />
+        ) : (
+          <meshBasicMaterial />
+        )}
       </mesh>
     </Canvas>
   );
 });
 
 export default App;
+
+function getDefaultVertexShader() {
+  return `
+#ifdef GL_ES
+precision mediump float;
+#endif
+
+
+void main() {
+  gl_Position = position;
+}
+`;
+}
+
+function getDefaultFragmentShader() {
+  return `#ifdef GL_ES
+precision mediump float;
+#endif
+
+uniform float u_time;
+
+void main() {
+	gl_FragColor = vec4(1.0,0.0,1.0,1.0);
+}
+`;
+}
